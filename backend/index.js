3c39f7ae8a5195b740ff078b114a955d981cfa83
archita-b <archita.bhattacharyya91@gmail.com>
2025-08-26 00:28:11 +0530

    setup deployment

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import issueRouter from "./routes/issue.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", issueRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
